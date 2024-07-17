3b74a89a9a8ec876369434490772b0fb565d3bc1
Ansh3931up <thebeliever39@gmail.com>
2024-07-18 02:33:43 +0530

    small change

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -26,7 +26,8 @@ function BlogCard({ data }) {
                     {truncateDescription(data?.description, 30)} {/* Limit description to 150 characters */}
                 </p>
                 
-                <button type="button" className="text-white bg-orange-500 hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">Read More</button>
+                <div className="flex flex-row justify-between"><button type="button" className="text-white bg-orange-500 hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">Read More</button>
+                <button type="button" className="text-white bg-orange-500 hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">Remove</button></div>
                 
             </div>
         </div>
