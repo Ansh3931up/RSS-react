f56309777a1368a0144bf33393f82f04b675cfe1
Ansh3931up <129533154+Ansh3931up@users.noreply.github.com>
2024-07-26 14:49:35 +0530

    Update CheckoutPage.jsx

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -70,33 +70,32 @@ function Checkout() {
     // console.log("key",razorpayKey)
 
     return (
-        <form onSubmit={handlePayment} className="min-h-[90vh] flex items-center justify-center text-white">
+ <form onSubmit={handlePayment}  className="min-h-[90vh] flex items-center justify-center bg-orange-100 text-orange-50">
             <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_black] rounded-lg relative">
-                <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg">Order Payment</h1>
+                <h1 className="bg-orange-500 absolute top-0 w-full text-center py-4 text-2xl font-bold rounded-tl-lg rounded-tr-lg">Order Payment</h1>
                 <div className="px-4 space-y-5 text-center">
-                    <p className="text-[17px]">
-                        This purchase will allow you to access all available courses
-                        on our platform for {" "} 
-                        <span className="text-yellow-500 font-bold">
+                    <p className="text-[17px] text-orange-500">
+                        This purchase will allow you to access our magazine hard copy
+                        on our platform 
+                        <span className="text-orange-500 font-bold">
                             <br />
-                            1 Year duration
+                    
                         </span> {" "}
-                        All the existing and new launched courses will be also available.
+                        All the existing and new launched magazine will be also available for sale too.
                     </p>
-                    <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
-                        <BiRupee /><span>{state.price}</span> only
+                    <p className="flex items-center justify-center gap-1 text-2xl font-bold text-orange-500">
+                        <BiRupee /><span>{state?.price}</span> only
                     </p>
-                    <div className="text-gray-200">
+                    <div className="text-orange-400">
                         <p>100% refund on cancellation</p>
                         <p>* Terms and conditions applied *</p>
                     </div>
-                    <button type="submit" className="bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full left-0 text-xl font-bold rounded-bl-lg rounded-br-lg py-2">
+                    <button type="submit" className="bg-orange-500 text-orange-50 hover:bg-yellow-600 transition-all ease-in-out duration-300 absolute bottom-0 w-full left-0 text-xl font-bold rounded-bl-lg rounded-br-lg py-2">
                         Buy now
                     </button>
                 </div>
             </div>
-        </form>
-    );
+        </form>    );
 }
 
 function generateReceipt() {
