1cadf7c2de51ec9a5dc34c27849ee92782afc79d
Ansh3931up <129533154+Ansh3931up@users.noreply.github.com>
2024-07-26 23:13:27 +0530

    Update CheckoutPage.jsx

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -23,7 +23,7 @@ function Checkout() {
         razorpay_payment_id: "",
         razorpay_order_id: "",
         razorpay_signature: "",
-        paycardid: {data._id}
+        paycardid: {state._id}
     };
 
     async function handlePayment(e) {
