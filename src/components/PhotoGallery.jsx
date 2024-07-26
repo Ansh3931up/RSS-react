7fd4aa20e6c76434b35432c7b2d5828703e7e95f
Ansh3931up <129533154+Ansh3931up@users.noreply.github.com>
2024-07-26 22:27:31 +0530

    Update PhotoGallery.jsx

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -16,11 +16,9 @@ function PhotoGallery() {
         dispatch(getphotos());
     }, [dispatch]);
 
-    // console.log(isLoggedIn, role);
-
     return (
         <div 
-            className="min-h-[90vh] pt-12 pl-4 md:pl-20 flex flex-col gap-10 text-white"
+            className="min-h-[90vh] pt-12 pl-4 pr-4 md:pl-20 flex flex-col gap-10 text-white"
             style={{ backgroundImage: `url(${updateImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
             <h1 className="text-3xl md:text-4xl font-bold">Gallery</h1>
@@ -31,7 +29,12 @@ function PhotoGallery() {
                 ))}
             </div>
             {isLoggedIn && role === "ADMIN" && (
-                <Link to="/upload/image" className="text-white bg-orange-500 w-full md:w-[11%] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">Upload Image</Link>
+                <Link 
+                    to="/upload/image" 
+                    className="text-white bg-orange-500 w-full md:w-1/3 lg:w-1/4 hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mb-4"
+                >
+                    Upload Image
+                </Link>
             )}
         </div>
     );
