b4937a14d5064739c6111dd600eebbf96a48e853
Ansh3931up <129533154+Ansh3931up@users.noreply.github.com>
2024-07-26 19:56:32 +0530

    Update BlogList.jsx

--- src/components/BlogPages/BlogList.jsx
+++ src/components/BlogPages/BlogList.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
 import { getblog } from "../../Redux/Blog";
 import BlogCard from "../BlogCard";
 import updateImage from "./update.jpg"; // Import the background image
@@ -16,22 +15,27 @@ function BlogList() {
         dispatch(getblog());
     }, [dispatch]);
 
-    console.log(isLoggedIn, role);
-
     return (
         <div 
-            className="min-h-[90vh] pt-12 pl-20 flex flex-col gap-10 text-white"
+            className="min-h-screen pt-12 px-4 md:px-20 flex flex-col gap-10 text-white"
             style={{ backgroundImage: `url(${updateImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-            <h1 className="text-4xl font-bold">Latest Updates</h1>
-            <span className="font-bold text-2xl">By RSS</span>
+            <h1 className="text-3xl md:text-4xl font-bold text-center">Latest Updates</h1>
+            <span className="font-bold text-xl md:text-2xl text-center">By RSS</span>
             <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {BlogData?.map((element) => (
                     <BlogCard key={element._id} data={element} />
                 ))}
             </div>
             {isLoggedIn && role === "ADMIN" && (
-             < Link to="/updates/create" className="text-white bg-orange-500 w-[11%] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">Create New Blog</Link>
+                <div className="text-center mt-8">
+                    <Link 
+                        to="/updates/create" 
+                        className="text-white bg-orange-500 hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center"
+                    >
+                        Create New Blog
+                    </Link>
+                </div>
             )}
         </div>
     );
